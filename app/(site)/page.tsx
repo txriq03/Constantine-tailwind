7f29aca805e766944cc57faf1974baa5368f122a
Tariq <118397532+txriq03@users.noreply.github.com>
2023-05-08 16:01:28 +0100

    🖌️👍 Added text gradiant animation

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -9,20 +9,20 @@ export default function Home() {
       <Container>
         <div className='text-white text-[7.5rem] font-bold tracking-tighter mt-20 whitespace-nowrap max-3xl:mt-5 max-md:text-center max-md:text-[5rem] max-sm:text-[3.5rem]'>
           <h1 >Drive with </h1>
-          <h1 className='bg-gradient-to-r from-seagreen to-blue-500 bg-clip-text text-transparent bg-[400%] text-[9rem] -mt-20 -mb-5 max-md:text-[7rem] max-md:text-center max-md:-mt-14 max-sm:text-[5rem] max-sm:-mt-10 '>Awareness</h1>
+          <h1 className='animate-text bg-gradient-to-r from-cyan-500 via-[#2ebf91] to-blue-500 bg-clip-text text-transparent bg-[400%] text-[9rem] -mt-20 -mb-5 max-md:text-[7rem] max-md:text-center max-md:-mt-14 max-sm:text-[5rem] max-sm:-mt-10'>Awareness.</h1>
         </div>
         <p className='font-semibold text-slate-300 text-[1.8rem] tracking-tighter w-[45rem] mb-5 max-md:text-[1.5rem] max-md:text-center max-md:mx-auto max-md:w-[35rem] max-sm:text-[1rem] max-sm:w-[20rem]'>
           Get yourself behind the wheel with the aid of our experienced driving instructors. You&apos;ll be able to pass your test with confidence.
         </p>
 
         {/*Button*/}
-        <a href="#_" className="relative mb-20 inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-seagreen bg-seagreen rounded-lg shadow-md group">
-          <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-seagreen group-hover:translate-x-0 ease">
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+        <a href="#_" className="relative p-0.5 mb-5 inline-flex items-center justify-center font-semibold overflow-hidden group rounded-md">
+          <span className="w-full h-full animate-text bg-gradient-to-br from-cyan-500 via-[#2ebf91] to-blue-500 group-hover:from-cyan-500 group-hover:via-[#2ebf91] group-hover:to-blue-500 absolute"></span>
+          <span className="relative px-6 py-3 transition-all ease-out bg-zinc-900 rounded-md group-hover:bg-opacity-0 duration-400">
+            <span className="relative text-white text-lg">Learn more</span>
           </span>
-          <span className="absolute flex items-center justify-center w-full h-full text-white text-lg transition-all duration-300 transform group-hover:translate-x-full ease">Start Driving</span>
-          <span className="relative invisible">Button Text</span>
         </a>
+        
         <Image className='object-contain absolute top-60 -right-80' alt='car' src={sideview} height={1000} width={1000} />
       </Container>
     </main>
